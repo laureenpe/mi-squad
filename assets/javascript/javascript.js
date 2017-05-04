ac677431054e4228b3c0c50390a0cf603faa03b5
“Laura <laureen.pelaez.isaza@gmail.com>
2017-05-04 17:45:57 -0300

    add a section to html & an instance to js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,10 +6,12 @@ function MiembroSquad(nombre, apellido, edad, hobbies) { //constructor creado co
 }
 //Creo una instancia por cada miembro de mi MiembroSquad
 //const es como una variable utilizada en el nuevo ecmascript 6 que se usa cuando el valor siempre es el mismo, no cambia 
-const Laura = new MiembroSquad("Laura", "Peláez", "undefined", "cinéfila")
-const Orieta = new MiembroSquad("Orieta", "Toro", "27", "Leer y dormir")
-const Constanza = new MiembroSquad("Constanza", "Pecori", "26", "tejer comer y dormir ")
-const Rangi = new MiembroSquad("Rangi", "Becerra", "32", "viajar")
-const Caterina = new MiembroSquad("Caterina", "Da Silva", "26", " juegos online")
-const Rosa = new MiembroSquad("Rosa", "Díaz", "32", "Pensar en la inmortalidad del cangrejo")
-const Genesis = new MiembroSquad("Genesis", "Narváez", "27", "pasear en bici.")
+const laura = new MiembroSquad("Laura", "Peláez", "undefined", "cinéfila")
+const orieta = new MiembroSquad("Orieta", "Toro", "27", "Leer y dormir")
+const constanza = new MiembroSquad("Constanza", "Pecori", "26", "tejer comer y dormir ")
+const rangi = new MiembroSquad("Rangi", "Becerra", "32", "viajar")
+const caterina = new MiembroSquad("Caterina", "Da Silva", "26", " juegos online")
+const rosa = new MiembroSquad("Rosa", "Díaz", "32", "Pensar en la inmortalidad del cangrejo")
+const genesis = new MiembroSquad("Genesis", "Narváez", "27", "pasear en bici.")
+
+console.log(laura.nombre);
